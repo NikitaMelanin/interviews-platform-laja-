7e1d8ec4888079f5dd476e3e658b6c894e784e13
HappyRoma <krivonogov.roma@mail.ru>
2022-06-07 01:31:16 +0500

    Add Vacancy's interviews list component

--- InterviewsPlatform-66bit/ClientApp/src/app/dashboard/dashboard.module.ts
+++ InterviewsPlatform-66bit/ClientApp/src/app/dashboard/dashboard.module.ts
@@ -6,10 +6,11 @@ import {VacancyComponent} from "./components/vacancy/vacancy.component";
 import {VacancyEditComponent} from "./components/vacancy-edit/vacancy-edit.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {CreateVacancyComponent} from "./components/create-vacancy/create-vacancy.component";
+import {VacanciesInterviewsListComponent} from "./components/vacancies-interviews-list/vacancies-interviews-list.component";
 
 
 @NgModule({
-  declarations: [DashboardComponent, VacancyComponent, VacancyEditComponent],
+  declarations: [DashboardComponent, VacancyComponent, VacancyEditComponent, VacanciesInterviewsListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
@@ -17,6 +18,7 @@ import {CreateVacancyComponent} from "./components/create-vacancy/create-vacancy
       {path: 'vacancy/create', component: CreateVacancyComponent, pathMatch: 'full'},
       {path: 'vacancy/:id/edit', component: VacancyEditComponent, pathMatch: 'full'},
       {path: 'vacancy/:id', component: VacancyComponent, pathMatch: 'full'},
+      {path: 'vacancy/:id/interviews-list', component: VacanciesInterviewsListComponent, pathMatch: 'full'}
     ]),
     ReactiveFormsModule,
   ]
