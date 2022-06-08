936fafbb557d43ef3312ce30b7bc247451c0123c
jexinox <jexinox@gmail.com>
2022-06-08 15:51:11 +0500

    questions

--- InterviewsPlatform-66bit/ClientApp/src/app/interview/services/questions-receiver.service.ts
+++ InterviewsPlatform-66bit/ClientApp/src/app/interview/services/questions-receiver.service.ts
@@ -6,9 +6,9 @@ import {HttpClient} from "@angular/common/http";
 export class QuestionsReceiver {
   constructor(private readonly httpClient: HttpClient) {}
 
-  async getQuestions(id: string): string[] {
+  async getQuestions(id: string): Promise<string[]> {
     // хз как
-    this.httpClient.get(`/interviews/${id}`).toPromise().then(x => {return x as string[]});
+    this.httpClient.get(`/api/interviews/${id}/questions`).toPromise().then(x => {return JSON.parse(x.toString()) as string[]});
     return [];
   }
 }
