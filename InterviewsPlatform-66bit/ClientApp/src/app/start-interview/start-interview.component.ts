4e3d98fa4535ef788f9ef206189d0d683ed79172
HappyRoma <krivonogov.roma@mail.ru>
2022-06-09 23:01:33 +0500

    start-interview updated, some visual fixes

--- InterviewsPlatform-66bit/ClientApp/src/app/start-interview/start-interview.component.ts
+++ InterviewsPlatform-66bit/ClientApp/src/app/start-interview/start-interview.component.ts
@@ -24,6 +24,7 @@ export class StartInterviewComponent implements OnInit, OnDestroy {
     this.httpClient.post('https://localhost:44423/api/vacancies/' + this.id + '/interviews', {
       name: this.myForm.value.name,
       surname: this.myForm.value.surname,
+      patronymic: this.myForm.value.patronymic,
       email: this.myForm.value.email,
       phone: this.myForm.value.phone,
     }).subscribe(x => {
@@ -36,12 +37,17 @@ export class StartInterviewComponent implements OnInit, OnDestroy {
     this.myForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       surname: new FormControl('', [Validators.required]),
+      patronymic: new FormControl('', [Validators.required]),
       phone: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.email, Validators.required]),
     })
   }
 
   ngOnDestroy(): void {
+  }
+
+  public cancel(): void {
+    this.myForm.reset();
   }
 
 }
